0e448885b91819a427a44c28abd13808c930dd32
Odhran Sexton <92513044+osexton1@users.noreply.github.com>
2022-03-24 17:41:05 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import { createApp } from "vue";
+import { createWebHistory, createRouter } from 'vue-router';
 import store from "./store";
 import App from "./App";
- 
+import AuthHandler from "./components/AuthHandler";
+
+const routes = [
+    {
+        path: '/oauth2/callback',
+        component: AuthHandler
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
 const app = createApp(App);
-app.mount("#app");
 app.use(store);
+app.use(router);
+app.mount("#app");
