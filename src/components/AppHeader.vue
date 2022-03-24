5907332b8c2862d9fa9e7353ffb7b02f619cce07
Odhran Sexton <92513044+osexton1@users.noreply.github.com>
2022-03-24 17:39:50 +0000

    Updated AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,11 +3,27 @@
         <a href="/" class="active item">
             Image Storage
         </a>
+        {{ isLoggedIn }}
+        <div class="right menu">
+            <div v-if="isLoggedIn">
+                Gallery
+                Upload
+                Logout
+            </div>
+            <!-- v-else MUST be placed immediately after an element with v-if -->
+            <a v-else href="#" class="ui item" @click="login">
+                Login
+            </a>
+        </div>
     </div>
 </template>
 
 <script>
+    import { mapActions, mapGetters } from 'vuex';
+
     export default {
-        name: 'AppHeader'
-    }
+        name: 'AppHeader',
+        computed: mapGetters(['isLoggedIn']),
+        methods: mapActions(['login'])
+    };
 </script>
