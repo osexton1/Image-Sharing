d6d22e95e72f51f1d4fd014288709226e9f280ca
Odhran Sexton <92513044+osexton1@users.noreply.github.com>
2022-03-24 14:15:38 +0000

    Added App.vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <AppHeader></AppHeader>
+</template>
+
+<script>
+    import AppHeader from './components/AppHeader'
+
+    export default {
+        name: 'App',
+        components: {
+            AppHeader
+        }
+    }
+</script>
