8d9ebc6d05fb7c033b399465cae1ed1a8fc35e25
Odhran Sexton <92513044+osexton1@users.noreply.github.com>
2022-03-24 17:44:51 +0000

    Added imgur.js

---
+++ src/api/imgur.js
@@ -0,0 +1,16 @@
+import qs from 'qs';
+
+const CLIENT_ID = 'ID left out for privacy - you need your own';
+const ROOT_URL = 'https://api.imgur.com';
+
+export default {
+    login() {
+        const querystring = {
+            client_id: CLIENT_ID,
+            response_type: 'token'
+        };
+
+        window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(querystring)}`;
+    }
+
+};
