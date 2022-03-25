2202874a768018be9eb323886272f1fe627f2bcf
Odhran Sexton <92513044+osexton1@users.noreply.github.com>
2022-03-25 16:12:43 +0000

    Updated AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,14 +1,15 @@
 <template>
     <div class="ui secondary pointing menu">
-        <a href="/" class="active item">
+        <!-- router-link is functionally the same as an 'a' tag, but doesn't cause a full
+             page refresh -->
+        <router-link to="/" class="active item">
             Image Storage
-        </a>
-        {{ isLoggedIn }}
+        </router-link>
         <div class="right menu">
-            <div v-if="isLoggedIn">
-                Gallery
-                Upload
-                Logout
+            <div v-if="isLoggedIn" class="horizontal">
+                <router-link to="/" class="item">Gallery</router-link>
+                <router-link to="/upload" class="item">Upload</router-link>
+                <router-link to="/" class="item" @click="logout">Logout</router-link>
             </div>
             <!-- v-else MUST be placed immediately after an element with v-if -->
             <a v-else href="#" class="ui item" @click="login">
@@ -24,6 +25,13 @@
     export default {
         name: 'AppHeader',
         computed: mapGetters(['isLoggedIn']),
-        methods: mapActions(['login'])
+        methods: mapActions(['login', 'logout'])
     };
 </script>
+
+<style scoped>
+    .horizontal {
+        display: flex;
+        flex-direction: row;
+    }
+</style>
