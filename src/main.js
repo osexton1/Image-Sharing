1d06b5e9ffbaf779e18a8e227b2627e7f269c1c9
Odhran Sexton <92513044+osexton1@users.noreply.github.com>
2022-03-25 16:19:47 +0000

    Updated main.js

--- src/main.js
+++ src/main.js
@@ -3,15 +3,25 @@ import { createWebHistory, createRouter } from 'vue-router';
 import store from "./store";
 import App from "./App";
 import AuthHandler from "./components/AuthHandler";
+import ImageList from "./components/ImageList";
+import UploadForm from "./components/UploadForm";
 
 const routes = [
     {
         path: '/oauth2/callback',
         component: AuthHandler
+    },
+    {
+        path: '/',
+        component: ImageList
+    },
+    {
+        path: '/upload',
+        component: UploadForm
     }
 ];
 
-const router = createRouter({
+export const router = createRouter({
     history: createWebHistory(),
     routes,
 });
